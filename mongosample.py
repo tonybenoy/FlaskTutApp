2d7c18aadf9dc9f5700ff24da0baed5c434ab255
Tony Benoy <me@tonybenoy.com>
2019-04-01 22:24:37 +0530

    Mongosample

--- mongosample.py
+++ mongosample.py
@@ -15,3 +15,87 @@ x = mycol.insert_one(mydict)
 mydict = { "name": "hp XPS 16", "Price": 1000,
         "url":"https://amzon.in/kjddsd","category":"internet","imgurl":"image url2" }
 x = mycol.insert_one(mydict)
+
+
+
+
+mycol = mydb["cpu"]
+#make sure the names matches everywhere and it is case sensitive
+mydict = {
+    "cpu" : "i5",
+    "name" : "Intel i5 8 gen",
+    "manufacturer" : "intel",
+    "url" : "www.google.com/nn",
+    "imgurl" : "www.google.com/nni",
+    "price" : "2000"
+}
+x = mycol.insert_one(mydict)
+mydict={
+    "cpu" : "i5",
+    "name" : "Intel i5 6 gen",
+    "manufacturer" : "intel",
+    "url" : "www.google.com/gg",
+    "imgurl" : "www.google.com/ggi",
+    "price" : "1000"
+}
+x = mycol.insert_one(mydict)
+mydict={
+    "cpu" : "i3",
+    "name" : "Intel i3 6 gen",
+    "manufacturer" : "intel",
+    "url" : "www.google.com/gg",
+    "imgurl" : "www.google.com/ggi",
+    "price" : "10000"
+}
+x = mycol.insert_one(mydict)
+mydict={
+    "cpu" : "i3",
+    "name" : "Intel i3 7 gen",
+    "manufacturer" : "intel",
+    "url" : "www.google.com/gg",
+    "imgurl" : "www.google.com/ggi",
+    "price" : "10090"
+}
+
+x = mycol.insert_one(mydict)
+mycol = mydb["ram"]
+mydict = {
+    "ram" : "2",
+    "name" : "Corsair 2GB",
+    "manufacturer" : "Corsair",
+    "url" : "www.google.com/gg",
+    "imgurl" : "www.google.com/ggi",
+    "price" : "1000"
+}
+x = mycol.insert_one(mydict)
+
+
+mydict = {
+    "ram" : "2",
+    "name" : "Kingston 2GB",
+    "manufacturer" : "Kingston",
+    "url" : "www.google.com/ggf",
+    "imgurl" : "www.google.com/fggi",
+    "price" : "1300"
+}
+x = mycol.insert_one(mydict)
+
+mydict ={
+    "ram" : "4",
+    "name" : "Corsair 4GB",
+    "manufacturer" : "Corsair",
+    "url" : "www.google.com/gg",
+    "imgurl" : "www.google.com/ggi",
+    "price" : "1000"
+}
+x = mycol.insert_one(mydict)
+
+mydict ={
+    "ram" : "4",
+    "name" : "Kingston 4GB",
+    "manufacturer" : "Kingston",
+    "url" : "www.google.com/ggf",
+    "imgurl" : "www.google.com/fggi",
+    "price" : "1300"
+}
+x = mycol.insert_one(mydict)
